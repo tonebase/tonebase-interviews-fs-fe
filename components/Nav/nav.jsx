eaf23b41e060baf1b4daa6436ce08591d151dbc2
Stanley <stanley.traub@gmail.com>
2020-05-26 20:36:22 -0700

    Refactor search icon

--- components/Nav/nav.jsx
+++ components/Nav/nav.jsx
@@ -13,8 +13,18 @@ const Nav = () => {
       <nav className="nav flex-row">
         <style jsx>{style}</style>
         <div className="nav__container flex-row">
-          <div className="nav__container-link">About</div>
-          <div className="nav__container-link">Store</div>
+          <a
+            href="https://about.google/?fg=1&utm_source=google-US&utm_medium=referral&utm_campaign=hp-header"
+            className="nav__container-link"
+          >
+            About
+          </a>
+          <a
+            href="https://store.google.com/US/?utm_source=hp_header&utm_medium=google_ooo&utm_campaign=GS100042&hl=en-US"
+            className="nav__container-link"
+          >
+            Store
+          </a>
         </div>
         <div className="nav__container flex-row">
           <div className="nav__container-theme">
@@ -25,15 +35,21 @@ const Nav = () => {
             isOn={value}
             onColor="#06D6A0"
             handleToggle={() => {
-                // dispatch an action to the theme reducer to switch between dark and light themes based on the current state
-                dispatch({ type: "SET_THEME" });
-                // changes value of current state of the toggle between 'true' and 'false'
-                setValue(!value);
+              // dispatch an action to the theme reducer to switch between dark and light themes based on the current state
+              dispatch({ type: "SET_THEME" });
+              // changes value of current state of the toggle between 'true' and 'false'
+              setValue(!value);
             }}
           />
           <div className="nav__container-link">Gmail</div>
           <div className="nav__container-link">Images </div>
-          <div className="nav__container-link">Stan</div>
+          <div className="nav__container__photo">
+            <img
+              src="../../static/images/Screen Shot 2020-05-26 at 6.46.14 PM.png"
+              alt="Profile picture"
+              className="nav__container__photo-img"
+            />
+          </div>
         </div>
       </nav>
     );
--- components/SearchForm/searchForm.jsx
+++ components/SearchForm/searchForm.jsx
@@ -1,17 +1,17 @@
-import React, { useState } from 'react'
+import React from 'react'
 import style from "../../sass/main.scss";
 
 const SearchForm = () => {
-    const [searchQuery, setQuery] = useState("");
-         
     return (
       <form className="search-form">
         <style jsx>{style}</style>
+        
         <input
           type="text"
           className="search-form__input"
           placeholder="Search Google or type a url"
         />
+
         <div className="buttons">
           <button className="buttons__btn-1 btn">Google Search</button>
           <button className="buttons__btn-2 btn">I'm Feeling Lucky</button>
