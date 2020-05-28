abca231608242ccc663472435608e7639591c4e2
Thomas Nelson <thnlsn@gmail.com>
2020-05-28 12:59:49 -0700

    exporting counter page with tonebase premade wrapper

--- pages/counter/index.js
+++ pages/counter/index.js
@@ -1,7 +1,12 @@
 import React, { Fragment } from 'react';
+import Head from 'next/head';
+
+// === PAGE WRAPPERS ===
+import { withFrame } from '../../components/PageWrappers';
+import { store, view } from 'react-easy-state';
 
 const Counter = () => {
   return <div></div>;
 };
 
-export default Counter;
+export default withFrame(view(Counter));
