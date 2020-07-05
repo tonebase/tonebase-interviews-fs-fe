66989f3355d3e5acb7c1342f5dffab977781b6a4
marcusbarnesdeveloper <marcusbarnesdev@gmail.com>
2020-07-04 22:53:28 -0700

    easy state react

--- pages/Home.js
+++ pages/Home.js
@@ -1,49 +1,37 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import '../styleSystem.scss';
-class Home extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      counter: 0
+import { store, view, autoEffect } from '@risingstack/react-easy-state';
+
+
+  const counter = store({
+    num: 0,
+    incrementCounter: () => counter.num++,
+    decrementCounter: () => counter.num--
+  });
+  autoEffect(() => {
+  var miliSecondsToHour = 3600000;
+  let onceAnHour = setInterval(() => {
+    var upHours = [9,10,11,12,13,14,15,16,17];
+    var downHours = [18,19,20,21,22,23,24,1,2,3,4,5,6,7,8];
+    var getHour = new Date().getHours();
+    if(downHours.includes(getHour)) {
+      counter.decrementCounter();
+    } else {
+      counter.incrementCounter();
     }
-    this.incrementCounter = this.incrementCounter.bind(this);
-    this.decrementCounter = this.decrementCounter.bind(this);
-  }
-  componentDidMount() {
-    var miliSecondsToHour = 3600000;
-    this.onceAnHour = setInterval(() => {
-      var upHours = [9,10,11,12,13,14,15,16,17];
-      var downHours = [18,19,20,21,22,23,24,1,2,3,4,5,6,7,8];
-      var getHour = new Date().getHours();
-      if(downHours.includes(getHour)) {
-        this.decrementCounter();
-      } else {
-        this.incrementCounter();
-      }
 
-    }, miliSecondsToHour);
-  }
-  componentWillUnmount() {
-    clearInterval(this.onceAnHour);
-  }
-  incrementCounter() {
-    this.setState({counter: this.state.counter + 1});
-  }
-  decrementCounter() {
-    this.setState({counter: this.state.counter - 1});
-  }
+  }, miliSecondsToHour);
+  return function(){ clearInterval(onceAnHour);}
+});
 
-  render() {
-    return (
-      <div style={{width: '50%',margin:'0 auto',textAlign:'center'}}>
-        {this.state.counter}
-        <div>
-          <button style={{marginRight:'1px', width: '50px'}} onClick={this.incrementCounter}>Up</button>
-          <button style={{marginLeft:'1px', width: '50px'}} onClick={this.decrementCounter}>Down</button>
-        </div>
-      </div>
-    )
-  }
-}
 
-export default Home;
+
+export default  view( () => (
+  <div style={{width: '50%',margin:'0 auto',textAlign:'center'}}>
+    {counter.num}
+    <div>
+      <button style={{marginRight:'1px', width: '50px'}} onClick={counter.incrementCounter}>Up</button>
+      <button style={{marginLeft:'1px', width: '50px'}} onClick={counter.decrementCounter}>Down</button>
+    </div>
+  </div>
+  ));
